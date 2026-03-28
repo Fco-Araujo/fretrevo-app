51f6971f7be8ad4cb64b0e66812c2c4e81e10815
Francisco José <josearaujo6425@gmail.com>
2026-03-28 17:38:36 +0000

    Alteração config.js render

--- controle-atividades/frontend/js/config.js
+++ controle-atividades/frontend/js/config.js
@@ -1,1 +1,8 @@
-export const API_BASE_URL = "https://supreme-space-carnival-jj7j7wjw5r54cq769-3000.app.github.dev";
+const isAmbienteLocal =
+  window.location.hostname === "127.0.0.1" ||
+  window.location.hostname === "localhost" ||
+  window.location.hostname.includes("github.dev");
+
+export const API_BASE_URL = isAmbienteLocal
+  ? "https://supreme-space-carnival-jj7j7wjw5r54cq769-3000.app.github.dev"
+  : "https://fretrevo-api.onrender.com";
